e3420fba91872fe0cb789484a31348152d3df7ae
Jun Nagai <junna9a1@gmail.com>
2019-06-14 10:32:19 +0800

    [[git-now]] Fri Jun 14 10:32:19 CST 2019
     mix.exs | 2 +-
     1 file changed, 1 insertion(+), 1 deletion(-)

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Parser.MixProject do
 
   def project do
     [
-      app: :parser,
+      app: :cs_parser,
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
